0f4b7c6a976838d3b612630ee4ee71c6d346dcea
Javier Bórquez <hi@javier.xyz>
2017-04-15 22:41:29 -0500

    Add UI

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# Click of death.
+# Control User Cursor.
+Small experiment to alter the cursor behavior.
+
+[![](docs_assets/screenshot.png)](http://javier.xyz/control-user-cursor/)
 
 ## How it works?
 Simple! I make the user cursor invisible, and then paint my own cursor with JS!
--- index.js
+++ index.js
@@ -1,55 +1,187 @@
+// config
+
+const kAssets = {
+  mac_retina: {
+    normal: {
+      src: 'assets/mac_retina.png',
+      height: '22px',
+      width: '15px'
+    },
+    pointer: {
+      src: 'assets/mac_pointer_retina.png',
+      width: '18.5px',
+      height: '19.5px'
+    }
+  },
+  mac: {
+    normal: {
+      src: 'assets/mac.png',
+      width: '15px',
+      height: '22px'
+    },
+    pointer: {
+      src: 'assets/mac_pointer.png',
+      width: '18px',
+      height: '19px'
+    }
+  },
+  other: {
+    normal: {
+      src: 'assets/other.png',
+      width: '17px',
+      height: '23px'
+    },
+    pointer: {
+      src: 'assets/other_pointer.png',
+      width: '22px',
+      height: '26px'
+    }
+  }
+};
+
+function getCursor() {
+  let navigator = window.navigator.platform.indexOf('Mac') > -1 ? 'mac' : 'win';
+
+  if (window.devicePixelRatio > 1) {
+    navigator += '_retina';
+  }
+
+  if (Object.keys(kAssets).includes(navigator)) {
+    return kAssets[navigator];
+  }
+
+  return kAssets.other;
+}
+
+function setCursor(cursorConfig) {
+  C.el.src = cursorConfig.src;
+  C.el.style.width = cursorConfig.width;
+  C.el.style.height = cursorConfig.height;
+}
+
 // global
 const C = {};
 
-C.el = document.querySelector('#cursor');
-C.kElementsToTrack = {
-  buttonToPrevent: {
-    el: document.querySelector('#buttonToPrevent'),
-    behavior: 'REPEL'
-  }
-};
+C.containerEl = document.querySelector('#container');
+C.el = document.createElement('img');
+C.el.id = 'cursor';
+
+C.cursorConfig = getCursor();
+setCursor(C.cursorConfig.normal);
+
+document.body.appendChild(C.el);
+
+C.elToTrack = {};
+C.elToTrackKeys = Object.keys(C.elToTrack);
+
 C.isMouseVisible = false;
 
-C.kElementsToTrackKeys = Object.keys(C.kElementsToTrack);
+function setup(config) {
+  const elConfig = config.el;
 
+  C.expoWeight = config.expoWeight;
+
+  C.containerEl.innerHTML = '';
+
+  C.elToTrack = elConfig;
+
+  C.elToTrackKeys = Object.keys(C.elToTrack);
+  C.elToTrackKeys.map(key => {
+    C.elToTrack[key].el = document.createElement('div');
+    C.elToTrack[key].el.className = elConfig[key].className.join(' ');
+    C.elToTrack[key].el.innerHTML = elConfig[key].innerHTML;
+    C.elToTrack[key]._hover = false;
+
+    C.containerEl.appendChild(C.elToTrack[key].el);
+  });
+
+  onUpdateElementSizes();
+}
+
+// UTILS
 function getPolarity(num) {
   return num >= 0 ? 1 : -1;
 }
 
 function diffAndAngleToXY(diff, angle, offset) {
+  const realOffset = offset > 4 ? Math.max(offset, 0) : 0;
+
   return {
-    x: Math.round(Math.cos(angle) * (diff + offset)),
-    y: Math.round(Math.sin(angle) * (diff + offset))
+    x: Math.round(Math.cos(angle) * (diff + realOffset)),
+    y: Math.round(Math.sin(angle) * (diff + realOffset))
   };
 }
 
+function getDiff(obj1, obj2) {
+  const diffX = obj1.x - obj2.x;
+  const diffY = obj1.y - obj2.y;
+  return Math.sqrt(diffX * diffX + diffY * diffY);
+}
+
+// does the math, using the elToTrack config object
 function calculateNewCursor(newCursor) {
   const calculatedCursor = {
     x: newCursor.x,
     y: newCursor.y
   };
 
-  C.kElementsToTrackKeys.map(key => {
-    const objSize = C.kElementsToTrack[key]._size;
-    const objCenter = C.kElementsToTrack[key]._center;
+  C.elToTrackKeys.map(key => {
+    const objSize = C.elToTrack[key]._size;
+    const objCenter = C.elToTrack[key]._center;
+    const objBehavior = C.elToTrack[key].behavior;
 
-    const diffX = objCenter.x - newCursor.x;
-    const diffY = objCenter.y - newCursor.y;
-    const diff = Math.sqrt(diffX * diffX + diffY * diffY);
+    const diff = getDiff(objCenter, newCursor);
 
-    const angle = Math.atan2(objCenter.y - newCursor.y, objCenter.x - newCursor.x);
+    const importance = Math.pow(1 - Math.min(1, Math.max(diff / C.wZ, 0)), C.expoWeight);
 
-    const importance = Math.max(Math.pow(1 - diff / C.wZ, 2), 0);
+    let xyDiff;
 
-    const xyDiff = diffAndAngleToXY(diff, angle, objSize * importance);
+    if (objBehavior === 'REPEL') {
+      const angle = Math.atan2(objCenter.y - newCursor.y, objCenter.x - newCursor.x);
 
-    calculatedCursor.x = Math.round(objCenter.x - xyDiff.x);
-    calculatedCursor.y = Math.round(objCenter.y - xyDiff.y);
+      xyDiff = diffAndAngleToXY(diff, angle, objSize * importance / 1.4142);
+    } else {
+      xyDiff = {
+        x: objCenter.x - (objCenter.x * importance + newCursor.x * (1 - importance)),
+        y: objCenter.y - (objCenter.y * importance + newCursor.y * (1 - importance))
+      };
+    }
+
+    calculatedCursor.x = calculatedCursor.x - newCursor.x + Math.round(objCenter.x - xyDiff.x);
+    calculatedCursor.y = calculatedCursor.y - newCursor.y + Math.round(objCenter.y - xyDiff.y);
   });
 
   return calculatedCursor;
 }
 
+// iterate over the elements to see if we we need to hover anyone
+function calculateHover(newCursor) {
+  C.elToTrackKeys.map(key => {
+    const trackedObj = C.elToTrack[key];
+
+    if (trackedObj.behavior !== 'ATRACT') {
+      return;
+    }
+
+    const calculatedDiff = getDiff(trackedObj._center, newCursor);
+
+    const isHovering = calculatedDiff < trackedObj._size / 2;
+
+    if (isHovering && trackedObj._hover === false) {
+      setCursor(C.cursorConfig.pointer);
+
+      trackedObj._hover = true;
+      trackedObj.el.classList.add('-hover');
+    } else if (!isHovering && trackedObj._hover === true) {
+      setCursor(C.cursorConfig.normal);
+
+      trackedObj._hover = false;
+      trackedObj.el.classList.remove('-hover');
+    }
+  });
+}
+
+// remove the fake cursor when the user moves the real out of the window
 function onMouseOut() {
   window.requestAnimationFrame(() => {
     C.el.style.opacity = 0;
@@ -57,6 +189,7 @@ function onMouseOut() {
   });
 }
 
+// main function that calculates the fake cursor position
 function onMouseMove(evt) {
   window.requestAnimationFrame(() => {
     if (C.isMouseVisible === false) {
@@ -69,6 +202,8 @@ function onMouseMove(evt) {
       y: evt.clientY
     });
 
+    calculateHover(calculatedCursor);
+
     C.el.style.left = calculatedCursor.x + 'px';
     C.el.style.top = calculatedCursor.y + 'px';
   });
@@ -79,19 +214,19 @@ function onUpdateElementSizes() {
     // update the window size values
     C.wX = window.innerWidth;
     C.wY = window.innerHeight;
-    C.wZ = Math.max(C.wX, C.wY) / 2;
+    C.wZ = Math.min(C.wX, C.wY) / 2;
 
     // iterate over all the elements that we are tracking, and update the clientRect value
-    C.kElementsToTrackKeys.map(key => {
-      const clientRect = C.kElementsToTrack[key].el.getBoundingClientRect();
+    C.elToTrackKeys.map(key => {
+      const clientRect = C.elToTrack[key].el.getBoundingClientRect();
 
       // update element internal vals
-      C.kElementsToTrack[key]._clientRect = clientRect;
-      C.kElementsToTrack[key]._center = {
+      C.elToTrack[key]._clientRect = clientRect;
+      C.elToTrack[key]._center = {
         x: Math.round(clientRect.left + clientRect.width / 2),
         y: Math.round(clientRect.top + clientRect.height / 2)
       };
-      C.kElementsToTrack[key]._size = Math.max(clientRect.width, clientRect.height);
+      C.elToTrack[key]._size = Math.max(clientRect.width, clientRect.height);
     });
   });
 }
@@ -100,6 +235,82 @@ window.addEventListener('resize', onUpdateElementSizes);
 window.addEventListener('mouseout', onMouseOut);
 window.addEventListener('contextmenu', event => event.preventDefault());
 
+Array.from(document.querySelectorAll('.-prevent-custom-cursor')).forEach(link => {
+  link.addEventListener('mouseover', evt => {
+    setCursor(C.cursorConfig.pointer);
+  });
+  link.addEventListener('mouseleave', evt => {
+    setCursor(C.cursorConfig.normal);
+  });
+});
+
 // initialize sizes
-onUpdateElementSizes();
+
+const kConfig = {
+  basic: {
+    el: {
+      buttonToPrevent: {
+        behavior: 'REPEL',
+        innerHTML: 'Nope :P',
+        className: ['clickme', '-nope']
+      }
+    },
+    expoWeight: 0.8
+  },
+  doble: {
+    el: {
+      buttonToPrevent: {
+        behavior: 'REPEL',
+        innerHTML: 'Nope :P',
+        className: ['clickme', '-nope', '-alt1']
+      },
+      buttonToPrevent2: {
+        behavior: 'REPEL',
+        innerHTML: 'Nope :P',
+        className: ['clickme', '-nope', '-alt2']
+      }
+    },
+    expoWeight: 2
+  },
+  atract: {
+    el: {
+      buttonToAtract: {
+        behavior: 'ATRACT',
+        innerHTML: 'Gravity!',
+        className: ['clickme']
+      }
+    },
+    expoWeight: 2.2
+  },
+  combined: {
+    el: {
+      buttonToPrevent: {
+        behavior: 'REPEL',
+        innerHTML: 'Nope :P',
+        className: ['clickme', '-nope', '-alt1']
+      },
+      buttonToAtract: {
+        behavior: 'ATRACT',
+        innerHTML: 'Gravity!',
+        className: ['clickme', '-alt2']
+      }
+    },
+    expoWeight: 2.2
+  }
+};
+
+setup(kConfig.basic);
 window.addEventListener('mousemove', onMouseMove);
+
+Array.from(document.querySelectorAll('.options-element')).forEach(option => {
+  console.log('adding listeners');
+
+  option.addEventListener('click', () => {
+    console.log('clickeooo');
+
+    document.querySelector('.options-element.-active').classList.remove('-active');
+    option.classList.add('-active');
+
+    setup(kConfig[option.dataset.type]);
+  });
+});
